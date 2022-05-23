a0a9420b0de304678e86e69d9e66ed7b7f16b2e4
Priscilla Afonso de Carvalho <priscilla.afcarvalho@gmail.com>
2022-05-23 17:39:43 +0100

    First commit - Java IDEs

---
+++ src/br/com/dio/PrimeiroProgramaJavaAulasDio.java
@@ -0,0 +1,25 @@
+package br.com.dio;
+
+import br.com.dio.model.Gatos;
+
+public class PrimeiroProgramaJavaAulasDio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Gatos gatos = new Gatos();
+		Livros livros = new Livros();
+		
+		System.out.println(gatos);
+		System.out.println(livros);
+		
+	}
+
+}
+
+
+class Livros {
+	/*private String nome;
+	private String npage;*/
+	
+}
